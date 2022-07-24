062c796ea624ede98331de634a4ba9be1cb5c6e4
sw0610 <enthdnjs11@gmail.com>
2022-07-24 22:39:39 +0900

    0724

---
+++ BOJ/src/Bronze/B3.java
@@ -0,0 +1,21 @@
+package Bronze;
+
+import java.util.Scanner;
+
+public class B3 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		for(int i=1;i<=n;i++) {
+			for(int j=0; j<n-i; j++) 
+				System.out.print(" ");
+			for(int k=1; k<=2*i-1;k++) 
+				System.out.print("*");
+			System.out.println();
+		}
+	}
+
+}
---
+++ BOJ/src/Bronze/test.java
@@ -0,0 +1,15 @@
+package Bronze;
+
+import java.util.Scanner;
+
+public class test {
+public static void main(String[] args) {
+
+
+	
+	int i1=Integer.MAX_VALUE;
+	int i2= i1+1;
+	System.out.println(i1);
+	System.out.println(i2);
+}
+}
