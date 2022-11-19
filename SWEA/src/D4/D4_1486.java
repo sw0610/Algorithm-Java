f253bfe00c724d98e33462379ac638853b919bba
두소원 <sowondoo@naver.com>
2022-11-19 14:40:48 +0900

    1486 장훈이의 높은 선반

--- SWEA/src/D4/D4_1486.java
+++ SWEA/src/D4/D4_1486.java
@@ -3,25 +3,80 @@ package D4;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class D4_1486 {
-	
+
 	static int N, B;
 	static int[] nums;
-	
+	static boolean[] selected;
+	static int res;
+
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
-		
+
 		int T = Integer.parseInt(br.readLine());
-		for(int tc=1;tc<=T;tc++) {
+		for (int tc = 1; tc <= T; tc++) {
 			st = new StringTokenizer(br.readLine());
-			N=Integer.parseInt(st.nextToken());
-			B=Integer.parseInt(st.nextToken());
-			
-			nums=new int[N];
+			N = Integer.parseInt(st.nextToken());
+			B = Integer.parseInt(st.nextToken());
+
+			nums = new int[N];
+			selected = new boolean[N];
+
+			st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < N; i++) {
+				nums[i] = Integer.parseInt(st.nextToken());
+			}
+
+//			Arrays.sort(nums);
+
+			res = Integer.MAX_VALUE;
+
+			subset(0);
+			sb.append("#" + tc + " " + res + "\n");
+
 		}
+		System.out.println(sb);
 	}
+
+	private static void subset(int idx) {
+		if (idx == N) {
+			int sum = 0;
+			for (int i = 0; i < N; i++) {
+				if (selected[i]) {
+					sum += nums[i];
+				}
+			}
+
+			if (sum < B) {
+				return;
+			} else {
+				res = Math.min(res, sum-B);
+
+				return;
+			}
+
+		}
+
+		selected[idx] = true;
+		subset(idx + 1);
+		selected[idx] = false;
+		subset(idx + 1);
+
+	}
+
+//	private void subset2(int idx, int sum) {
+//		
+//		if(sum>)
+//		
+//		selected[idx]=true;
+//		subset2(idx+1, sum+nums[idx]);
+//		selected[idx]=false;
+//		subset2(idx+1, sum);
+//		
+//	}
 }
